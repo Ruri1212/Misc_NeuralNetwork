dbc74d892d816941ed79da8903de1469ddf1f830
Ruri1212 <rikuinoue1007@icloud.com>
2024-02-06 15:04:00 +0900

    mlflow_clinet 実行結果を管理

--- image_process/client.py
+++ image_process/client.py
@@ -1,8 +1,32 @@
+## mlflowの結果を読み込み，必要に応じてソート
+## 任意の結果を表示するコード
+
+import os
+import time
+import copy
+
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import TensorDataset, DataLoader
+from torchvision import datasets, models, transforms
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+from omegaconf import OmegaConf, open_dict,DictConfig
+import mlflow
+from mlflow.models import infer_signature
 
 
-# client = mlflow.tracking.MlflowClient()
-# experiment_id = "878525502675615922"
-# best_run = client.search_runs(
-#     experiment_id, order_by=["metrics.val_loss"], max_results=1
-# )[0]
-# log.info(f"best_run: {best_run}")
+client = mlflow.tracking.MlflowClient()
+experiment_id = "878525502675615922"
+best_run = client.search_runs(
+    experiment_id, order_by=["metrics.val_loss"], max_results=1
+)[0]
+print(best_run)
